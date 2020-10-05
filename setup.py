30d5742087fea87c988a7b340417d9a66ead7567
Johnny Greco <jgreco.astro@gmail.com>
2020-10-05 17:48:21 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='artpop',
+      version='0.1',
+      author='Johnny Greco & Shany Danieli',
+      author_email='artpopcode@gmail.com',
+      packages=['artpop'],
+      url='https://github.com/ArtificialStellarPopulations/ArtPop')
