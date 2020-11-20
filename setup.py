22d19728a1d9ee18d146824741e78c391344909b
Johnny Greco <jgreco.astro@gmail.com>
2020-11-20 17:02:20 -0500

    readthedocs setup update

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python 
-
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    PATH = '/home/docs/checkouts/readthedocs.org/user_builds/artpop/conda/latest/bin/'
+    env = os.environ.copy()
+    env['PATH'] = env.get('PATH', "") + ":" + PATH
+else:
+    env = None
+
 
 setup(name='artpop',
       version='0.1',
