af6e4ea7d11522885c068e2c7d7b2dd6ee7dcbb6
Johnny Greco <jgreco.astro@gmail.com>
2020-11-21 17:50:26 -0500

    rtd damn it

--- artpop/__init__.py
+++ artpop/__init__.py
@@ -3,10 +3,10 @@ project_dir = os.path.dirname(os.path.dirname(__file__))
 package_dir = os.path.join(project_dir, 'artpop')
 data_dir = os.path.join(package_dir, 'data')
 
+from . import util
 from . import stars
 from . import space
 from . import image 
-from . import util
 from .filters import *
 from .viz import *
 from .image import *
