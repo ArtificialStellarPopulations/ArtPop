267765c0260d0644dd7b78052d899e40a50fc5a2
Johnny Greco <jgreco.astro@gmail.com>
2020-11-21 18:29:24 -0500

    rtd damn it

--- artpop/__init__.py
+++ artpop/__init__.py
@@ -1,7 +1,9 @@
 from . import util
 from .filters import *
 from .viz import *
-from .image import *
-from .stars import *
-from .space import *
 from .source import *
+from .image import  IdealImager, ArtImager, gaussian_psf, moffat_psf
+from .space import xy_from_grid, sersic_xy, plummer_xy
+from .stars.imf import *
+from .stars.populstions import *
+from .stars.isochrones import *
