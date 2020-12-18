baa819e05b445dee36557994fa0514adb66f48a5
Irvin Samuel <irvin@irvins-air.attlocal.net>
2020-12-17 20:13:40 -0800

    Added warning message if sample grid exceeds 1 gigabyte in memory usage

--- src/artpop/space/sample.py
+++ src/artpop/space/sample.py
@@ -1,4 +1,4 @@
-# Third-party 
+# Third-party
 import numpy as np
 from astropy import units as u
 from astropy.modeling.models import Sersic2D
@@ -20,35 +20,43 @@ def xy_from_grid(num_stars, model, xy_dim, sample_dim=None,
     ----------
     num_stars : int
         Number of stars (i.e., positions) to sample.
-    model : `~astropy.modeling.Fittable2DModel` 
-        A two-dimensional ``astropy`` model or a callable object that takes 
+    model : `~astropy.modeling.Fittable2DModel`
+        A two-dimensional ``astropy`` model or a callable object that takes
         x and y grid positions arguments: `model(xx, yy)`.
     xy_dim : int or list-like
         Dimensions of the mock image in xy coordinates. If int is given,
         will make the x and y dimensions the same.
     sample_dim : int or None, optional
-        Square dimension to sample within. This is useful if the mock image 
-        size is small compared to the scale of the model. To be useful, 
+        Square dimension to sample within. This is useful if the mock image
+        size is small compared to the scale of the model. To be useful,
         `sample_dim` should be larger than `xy_dim`.
     random_state : `None`, int, list of ints, or `~numpy.random.RandomState`
         If `None`, return the `~numpy.random.RandomState` singleton used by
         ``numpy.random``. If `int`, return a new `~numpy.random.RandomState`
         instance seeded with the `int`.  If `~numpy.random.RandomState`,
         return it. Otherwise raise ``ValueError``.
-    
+
     Returns
     -------
     xy : `~numpy.ma.MaskedArray`
-        Masked numpy array of xy positions. Positions that fall outside the 
+        Masked numpy array of xy positions. Positions that fall outside the
         mock image are masked.
     """
     xy_dim = check_xy_dim(xy_dim)
+
+    num_pix = 2 * sample_dim**2
+    byte_per_pixel = 8
+    memory_in_GB = (byte_per_pixel * num_pix) / 1e9
+    if memory_in_GB > 1:
+        msg = "Warning: This image will exceed 1 Gb in memory usage."
+        logger.warning(msg)
+
 
     if sample_dim is None:
         sample_dim = max(xy_dim)
 
     x_0, y_0 = sample_dim//2, sample_dim//2
-    yy, xx = np.meshgrid(np.arange(sample_dim), 
+    yy, xx = np.meshgrid(np.arange(sample_dim),
                          np.arange(sample_dim))
 
     image = model(xx, yy)
@@ -72,26 +80,26 @@ def xy_from_grid(num_stars, model, xy_dim, sample_dim=None,
         outside_image = x < 0
         outside_image |= x > xy_dim[0] - 1
         outside_image |= y < 0
-        outside_image |= y > xy_dim[1] - 1 
+        outside_image |= y > xy_dim[1] - 1
 
         if outside_image.sum() > 0:
             msg = '{} stars outside the image'.format(outside_image.sum())
             logger.warning(msg)
             xy.mask = np.column_stack((outside_image, outside_image))
-
+        
     return xy
 
 
-def sersic_xy(num_stars, r_eff, n, theta, ellip, distance, xy_dim, 
+def sersic_xy(num_stars, r_eff, n, theta, ellip, distance, xy_dim,
               pixel_scale=0.2, num_r_eff=10, random_state=None):
     """
-    Sample xy positions from a two-dimensional Sersic distribution. 
+    Sample xy positions from a two-dimensional Sersic distribution.
 
 
     .. note::
-        To sample the Sersic distribution, `~artpop.space.sample.xy_from_grid` 
-        is used. This means that large sources (and/or large `num_r_eff`) eat 
-        up a lot of memory. 
+        To sample the Sersic distribution, `~artpop.space.sample.xy_from_grid`
+        is used. This means that large sources (and/or large `num_r_eff`) eat
+        up a lot of memory.
 
     Parameters
     ----------
@@ -115,7 +123,7 @@ def sersic_xy(num_stars, r_eff, n, theta, ellip, distance, xy_dim,
         will make the x and y dimensions the same.
     pixel_scale : float or `~astropy.units.Quantity`, optional
         The pixel scale of the mock image. If a float is given, the units will
-        be assumed to be `~astropy.units.arcsec` per `~astropy.units.pixels`. 
+        be assumed to be `~astropy.units.arcsec` per `~astropy.units.pixels`.
     num_r_eff : float, optional
         Number of r_eff to sample positions within. This parameter is needed
         because the current Sersic sampling function samples from within a
@@ -129,7 +137,7 @@ def sersic_xy(num_stars, r_eff, n, theta, ellip, distance, xy_dim,
     Returns
     -------
     xy : `~numpy.ma.MaskedArray`
-        Masked numpy array of xy positions. Positions that fall outside the 
+        Masked numpy array of xy positions. Positions that fall outside the
         mock image are masked.
     """
     if n <= 0:
@@ -141,7 +149,7 @@ def sersic_xy(num_stars, r_eff, n, theta, ellip, distance, xy_dim,
     theta = check_units(theta, 'deg').to('radian').value
     distance = check_units(distance, 'Mpc').to('Mpc').value
     pixel_scale = check_units(pixel_scale, u.arcsec / u.pixel)
-    
+
     if r_eff <= 0:
         raise Exception('Effective radius must be greater than zero.')
 
@@ -150,20 +158,20 @@ def sersic_xy(num_stars, r_eff, n, theta, ellip, distance, xy_dim,
     sample_dim = 2 * np.ceil(r_pix * num_r_eff).astype(int) + 1
     x_0, y_0 = sample_dim//2, sample_dim//2
 
-    model = Sersic2D(x_0=x_0, y_0=y_0, amplitude=1, r_eff=r_pix, 
+    model = Sersic2D(x_0=x_0, y_0=y_0, amplitude=1, r_eff=r_pix,
                      n=n, ellip=ellip, theta=theta)
 
-    xy = xy_from_grid(num_stars, model, xy_dim, sample_dim, 
+    xy = xy_from_grid(num_stars, model, xy_dim, sample_dim,
                       random_state=random_state)
 
     return xy
 
 
-def plummer_xy(num_stars, scale_radius, distance, xy_dim, pixel_scale=0.2, 
+def plummer_xy(num_stars, scale_radius, distance, xy_dim, pixel_scale=0.2,
                random_state=None):
     """
-    Sample xy positions from a two-dimensional Plummer distributions using 
-    inverse transform sampling. 
+    Sample xy positions from a two-dimensional Plummer distributions using
+    inverse transform sampling.
 
     Parameters
     ----------
@@ -180,7 +188,7 @@ def plummer_xy(num_stars, scale_radius, distance, xy_dim, pixel_scale=0.2,
         will make the x and y dimensions the same.
     pixel_scale : float or `~astropy.units.Quantity`, optional
         The pixel scale of the mock image. If a float is given, the units will
-        be assumed to be `~astropy.units.arcsec` per `~astropy.units.pixels`. 
+        be assumed to be `~astropy.units.arcsec` per `~astropy.units.pixels`.
     random_state : `None`, int, list of ints, or `~numpy.random.RandomState`
         If `None`, return the `~numpy.random.RandomState` singleton used by
         ``numpy.random``. If `int`, return a new `~numpy.random.RandomState`
@@ -190,7 +198,7 @@ def plummer_xy(num_stars, scale_radius, distance, xy_dim, pixel_scale=0.2,
     Returns
     -------
     xy : `~numpy.ma.MaskedArray`
-        Masked numpy array of xy positions. Positions that fall outside the 
+        Masked numpy array of xy positions. Positions that fall outside the
         mock image are masked.
     """
     xy_dim = check_xy_dim(xy_dim)
@@ -202,13 +210,13 @@ def plummer_xy(num_stars, scale_radius, distance, xy_dim, pixel_scale=0.2,
     pixel_scale = check_units(pixel_scale, u.arcsec / u.pixel)
 
     s = rng.random(size=int(num_stars))
-    r = u.arcsec * r_sky / np.sqrt(s**(-2 / 3) - 1) 
+    r = u.arcsec * r_sky / np.sqrt(s**(-2 / 3) - 1)
     r = r.to('pixel', u.pixel_scale(pixel_scale)).value
 
     phi = rng.uniform(0, 2 * np.pi, size=len(r))
     theta = np.arccos(2 * rng.random(size=len(r)) - 1)
     xy = np.zeros((len(r), 2))
-    
+
     shift = xy_dim // 2
     xy[:, 0] = r * np.cos(phi) * np.sin(theta) + shift[0]
     xy[:, 1] = r * np.sin(phi) * np.sin(theta) + shift[1]
@@ -242,15 +250,15 @@ def uniform_xy(num_stars, xy_dim, random_state=None):
         ``numpy.random``. If `int`, return a new `~numpy.random.RandomState`
         instance seeded with the `int`.  If `~numpy.random.RandomState`,
         return it. Otherwise raise ``ValueError``.
-    
+
     Returns
     -------
     xy : `~numpy.array`
-        xy pixel positions. 
+        xy pixel positions.
     """
     xy_dim = check_xy_dim(xy_dim)
-    rng = check_random_state(random_state)  
+    rng = check_random_state(random_state)
     num_stars = int(num_stars)
-    xy = np.vstack([rng.uniform(0, xy_dim[0], num_stars), 
+    xy = np.vstack([rng.uniform(0, xy_dim[0], num_stars),
                     rng.uniform(0, xy_dim[1], num_stars)]).T
     return xy
